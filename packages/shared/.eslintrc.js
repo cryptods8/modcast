48a8fd9d160279210a9be4006aad537e74c97840
cryptods8 <dusoo.s+ds8@gmail.com>
2024-09-05 23:14:15 +0200

    fix eslint

--- packages/shared/.eslintrc.js
+++ packages/shared/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@xframes/eslint-config/react-internal.js"],
+  extends: ["@modcast/eslint-config/react-internal.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.json",
